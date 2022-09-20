79a4dced5f7ab86e5f54253e9bfb6ba849ecb698
Tyler Watson <tylerww91@gmail.com>
2022-09-20 16:48:02 -0700

    adds user display logic, if user hp is above 0, user img is upright, if user is HP is 0 user img shifts 90degrees and is darker

--- app.js
+++ app.js
@@ -2,10 +2,32 @@
 
 /* Get DOM Elements */
 
+const stat = document.getElementById('stat');
+const userImg = document.getElementById('user-img');
+const userHp = document.getElementById('user-hp');
+
 /* State */
+let user = {
+    hp: '30',
+    type: 'mario',
+};
+
+let opp = '';
 
 /* Events */
 
 /* Display Functions */
 
+function userDisplay() {
+    userHp.textContent = user.hp;
+    if (user.hp <= 0) {
+        userImg.src = 'assets/' + 'paper-mario-dead.png';
+    } else {
+        userImg.src = 'assets/' + 'paper-mario.png';
+    }
+}
+
+function displayOpp() {}
+
 // (don't forget to call any display functions you want to run on page load!)
+userDisplay();
