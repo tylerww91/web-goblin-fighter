72334b0afc4c2935428a7b052d5d4ca6980da90f
Tyler Watson <tylerww91@gmail.com>
2022-09-20 19:41:27 -0700

    adds the display of opponents/goblins using the renderfunction showing name, HP, and their portrait when alive, and a tomb stone when dead when changing hp values in state

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 /* Imports */
-
+import { renderOpp } from './render-opp.js';
 /* Get DOM Elements */
 
 const stat = document.getElementById('stat');
 const userImg = document.getElementById('user-img');
 const userHp = document.getElementById('user-hp');
+const battleLog = document.getElementById('battle-log');
+
+const oppSection = document.getElementById('opp-list');
 
 /* State */
 let user = {
     hp: '30',
-    type: 'mario',
+    name: 'mario',
+    status: 'alive',
 };
 
-let gameState = '';
+let opps = [
+    { name: 'Goombella', hp: 5 },
+    { name: 'Rex', hp: 15 },
+    { name: 'Cloudguy', hp: 10 },
+    { name: 'block', hp: 20 },
+];
 
-let opp = '';
+let message = '';
+
+// let gameState = '';
+
+// let opp = '';
 
 /* Events */
 
@@ -23,15 +36,28 @@ let opp = '';
 function userDisplay() {
     userHp.textContent = user.hp;
     if (user.hp <= 0) {
-        user = 'dead';
+        user.status = 'dead';
         userImg.src = 'assets/' + 'paper-mario-dead.png';
     } else {
         userImg.src = 'assets/' + 'paper-mario.png';
-        user = 'alive';
+        user.status = 'alive';
     }
 }
 
-function displayOpp() {}
+function displayOpp() {
+    oppSection.innerHTML = '';
+
+    for (const opp of opps) {
+        const oppEl = renderOpp(opp);
+        oppSection.append(oppEl);
+    }
+}
+
+function displayMessage() {
+    battleLog.textContent = `Earn stars by defeating opponents!`;
+}
 
 // (don't forget to call any display functions you want to run on page load!)
 userDisplay();
+displayMessage();
+displayOpp();
