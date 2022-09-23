5a655a1630c6dda765f62503ebd107087bb9d1f2
Tyler Watson <tylerww91@gmail.com>
2022-09-22 19:08:55 -0700

    adds game logic, user fights opponent, random damage dealt, user and goblin's hp updated, and a battle log detailing the damage dealt as well as a message for when an opponent is defeated

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 /* Imports */
 import { renderOpp } from './render-opp.js';
-// import { getRandomItem } from './utils.js';
+import { getRandomItem } from './utils.js';
 /* Get DOM Elements */
 
 // const stat = document.getElementById('stat');
 const userImg = document.getElementById('user-img');
 const userHp = document.getElementById('user-hp');
-// const battleLog = document.getElementById('battle-log');
+const battleLog = document.getElementById('battle-log');
+// const resultDisplay = document.getElementById('result-display');
 const messageDisplay = document.getElementById('message-display');
-
 const oppSection = document.getElementById('opp-list');
+const userKills = document.getElementById('user-kills');
 
 /* State */
-// let result = '';
+let result = '';
 
-// let defeated = '',
+let defeated = 0;
 
 let user = {
     hp: '30',
@@ -29,55 +30,55 @@ let opps = [
     { name: 'Kratos', type: 'whomp', hp: 13 },
 ];
 
-// const rex = {
-//     type: 'rex',
-//     hp: 10,
-// };
+const rex = {
+    type: 'rex',
+    hp: 10,
+};
 
-// const lakitu = {
-//     type: 'lakitu',
-//     hp: 8,
-// };
+const lakitu = {
+    type: 'lakitu',
+    hp: 8,
+};
 
-// const goombella = {
-//     type: 'goombella',
-//     hp: 3,
-// };
+const goombella = {
+    type: 'goombella',
+    hp: 3,
+};
 
-// const whomp = {
-//     type: 'whomp',
-//     hp: 13,
-// };
+const whomp = {
+    type: 'whomp',
+    hp: 13,
+};
 
-// const bowser = {
-//     type: 'bowser',
-//     hp: 40,
-// };
+const bowser = {
+    type: 'bowser',
+    hp: 40,
+};
 
-// const oppTypes = [
-//     goombella,
-//     goombella,
-//     goombella,
-//     goombella,
-//     lakitu,
-//     lakitu,
-//     lakitu,
-//     lakitu,
-//     lakitu,
-//     lakitu,
-//     rex,
-//     rex,
-//     rex,
-//     rex,
-//     rex,
-//     whomp,
-//     whomp,
-//     whomp,
-//     bowser,
-// ];
+const oppTypes = [
+    goombella,
+    goombella,
+    goombella,
+    goombella,
+    lakitu,
+    lakitu,
+    lakitu,
+    lakitu,
+    lakitu,
+    lakitu,
+    rex,
+    rex,
+    rex,
+    rex,
+    rex,
+    whomp,
+    whomp,
+    whomp,
+    bowser,
+];
 
-// const userAttacks = [0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5, 8];
-// const oppAttacks = [0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3];
+const userAttacks = [0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5, 8];
+const oppAttacks = [0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3];
 
 // let message = '';
 
@@ -88,6 +89,14 @@ let opps = [
 /* Events */
 
 /* Display Functions */
+
+function displayBattleLog() {
+    battleLog.textContent = result;
+}
+
+function displayMessage() {
+    messageDisplay.textContent = `Earn stars by defeating opponents! Click on an opponent to start fighting!`;
+}
 
 function userDisplay() {
     userHp.textContent = user.hp;
@@ -107,16 +116,44 @@ function displayOpp() {
         const oppEl = renderOpp(opp);
         oppSection.append(oppEl);
 
-        // oppEl.addEventListener('click', () => {
-        //     if (opp.hp < 1)
-        //     result = 'no use hitting a tombstone, that is just disrespectful';
+        oppEl.addEventListener('click', () => {
+            if (opp.hp < 1) {
+                result = 'no use hitting a tombstone, that is just disrespectful';
+                displayBattleLog();
+                return;
+            }
 
-        // })
+            const userAttack = getRandomItem(userAttacks);
+            const oppAttack = getRandomItem(oppAttacks);
+
+            user.hp = Math.max(0, user.hp - oppAttack);
+            opp.hp = Math.max(0, opp.hp - userAttack);
+
+            result = '';
+            if (userAttack === 0) {
+                result += 'Thatsaaa MISS';
+            } else {
+                result += `You hit ${opp.name} and dealt ${userAttack} damage!`;
+            }
+
+            if (oppAttack === 0) {
+                result += `${opp.name} missed ya!`;
+            } else {
+                result += `OUCH! ${opp.name} dealt you ${oppAttack} damage!`;
+            }
+
+            if (opp.hp < 1) {
+                defeated++;
+                displayBattleLog();
+                userKills.textContent = defeated;
+                messageDisplay.textContent = `You defeated ${defeated} opponent(s)`;
+            }
+
+            displayBattleLog();
+            userDisplay();
+            displayOpp();
+        });
     }
-}
-
-function displayMessage() {
-    messageDisplay.textContent = `Earn stars by defeating opponents! Click on an opponent to start fighting!`;
 }
 
 // (don't forget to call any display functions you want to run on page load!)
